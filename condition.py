3c10dcf8e3c22086939a71640efa9de2198139a6
ErickAlejandro <acea1234@hotmail.com>
2022-03-05 16:28:25 -0500

    Conditionals

--- condition.py
+++ condition.py
@@ -1,6 +1,14 @@
-edad = int(input("Cual es tu edad wacho?: "))
+# edad = int(input("Cual es tu edad wacho?: "))
 
-if edad > 17:
-    print("Tienes la obligacion de votar")
+# if edad > 17:
+#     print("Tienes la obligacion de votar")
+# else:
+#     print("tu eliges si quieres votar")
+numero = int(input("Escribe un numero:"))
+
+if numero > 5:
+    print("Este numero es mayor que 5")
+elif numero == 5:
+    print("Tu tienes un numero 5 aqui")
 else:
-    print("tu eliges si quieres votar")
+    print("Este numero es menor que 5")
--- conversor.py
+++ conversor.py
@@ -1,7 +1,38 @@
-pesos = input("Por favor, ingrese el valor: ")
-pesos = float(pesos)
-valor_dolar = 3975
-dolares = pesos / valor_dolar
-dolares = round(dolares, 2)
-dolares = str(dolares)
-print("Tienes $ " + dolares + "dólares")
+menu = """
+Bienvenido al conversor de monedas ✔💫
+Opcion1: Pesos Colombianos
+Opcion2: Pesos Argentinos
+Opcion3: Pesos Mexicanos
+
+Elige una opcion: 
+"""
+
+option = int(input(menu))
+
+if option == 1:
+    pesos = input("Por favor, ingrese el valor: ")
+    pesos = float(pesos)
+    valor_dolar = 3875
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $ " + dolares + "dólares")
+elif option == 2:
+    pesos = input("Por favor, ingrese el valor: ")
+    pesos = float(pesos)
+    valor_dolar = 65
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $ " + dolares + "dólares")
+elif option == 3:
+    pesos = input("Por favor, ingrese el valor: ")
+    pesos = float(pesos)
+    valor_dolar = 24
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $ " + dolares + "dólares")
+else:
+    print("Ingresa una opcion correcta.")
+
