f478f9cab913b5f6083a11e572c899a1a3689949
ErickAlejandro <acea1234@hotmail.com>
2022-03-14 11:30:11 -0500

    Added method to cut the image

--- method_fund_boxes.py
+++ method_fund_boxes.py
@@ -9,7 +9,6 @@ def convert_img_to_array(coordinates, img):
         datos = ''.join(t.readlines()).replace('\n', ';')
 
     m = np.matrix(datos)
-    m = np.delete(m , 0, axis=1)
     print(m)
 
     # Extraer las dimensiones de la imagen a usar
@@ -17,13 +16,27 @@ def convert_img_to_array(coordinates, img):
     y, x = image.shape[:2]
     print('El alto es Y: ' + str(y) + ' y el ancho es X: ' + str(x))
 
-    # Tranformacion de pixel para ubicar el punto medio de los BOXES
+    # Tranformacion de pixel para ingresar los datos en el OCR
     i = 1
     for i in range(len(m)):
-        print('Ancho en escala 0 - 1 en \n Y: ' + str(m[i , 1]) + ' \n X:' + str(m[i, 2]))
+        print('Fila de contador numero ' + str(i))
+        print('Ancho en escala 0 - 1 en \n alto: ' + str(m[i , 1]) + ' \nancho:' + str(m[i, 2]) + '\nTercer columna: ' + str(m[i, 3]) + '\nCuarta columna: ' + str(m[i, 4]) + '\n')
         transform_ruler3_y = y * m[i, 1]
         transform_ruler3_x = x * m[i, 2]
-        print('Tamaño del Y: ' + str(transform_ruler3_y) + ' PX \n Tamaño del ancho X: ' + str(transform_ruler3_x) + " PX \n")
+        transform_ruler3_3 = y * m[i, 3]
+        transform_ruler3_4 = x * m[i, 4]
+
+        print('Tamaño del alto en Y: ' + str(transform_ruler3_y) + ' PX \nTamaño del ancho X: ' + str(transform_ruler3_x) + " PX \nInformacion en pixels de la tercera columna: " + str(transform_ruler3_3) + 'PX \nInformacion en pixels de la cuarta columna: ' + str(transform_ruler3_4) + 'PX \n')
+
+        # OBTENER LA IMAGEN RECORTADA CON LAS COORDENADAS OBTENIDAS
+        pixelsY = int(y * m[i, 1])
+        pixelsX = int(x * m[i, 2])
+        pixelsH = int(y * m[i, 3])
+        pixelsW = int(x * m[i, 4])
+        
+        image_out = image[pixelsY:pixelsX, pixelsH:pixelsW]
+        cv.imshow('Imagen recortada', image_out)
+        cv.waitKey(0)
         i += 1
 
 
