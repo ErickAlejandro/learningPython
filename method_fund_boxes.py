71524c46af159466089a4498de3e74a0925c44b1
ErickAlejandro <acea1234@hotmail.com>
2022-03-25 15:05:02 -0500

    odified file

--- method_fund_boxes.py
+++ method_fund_boxes.py
@@ -1,23 +1,32 @@
 from math import ceil
 import pytesseract as pyt
 import torch
+from matplotlib import pyplot as plt
+import cv2
+from PIL import Image
 
 
 def ocr(bbox):
     # Lectura de los datos con Pytesseract
+    print(bbox.shape)
+    print(type(bbox))
+    im = Image.fromarray(bbox)
+    im.save("your_file.jpeg")
+    custom_oem_psm_config = r'--oem 3 --psm 6'
     text = pyt.image_to_string(bbox)
-    text = text.strip('\n\x0c')
-    text = text.replace('\n', ' ')
-    text = text.replace(' -', '-')
-    text = text.replace('- ', '')
-    text = text.replace('&L ', '')
-    print('Informacion de la fila: ' + str(text))
+    #text = text.strip('\n\x0c')
+    #text = text.replace('\n', ' ')
+    #text = text.replace(' -', '-')
+    #text = text.replace('- ', '')
+    #text = text.replace('&L ', '')
+    print('Informacion de la fila: ' + text)
     return text
 
 
 
 def convert_img_to_array(img):
     # Toma la imagen desde el webservice
+    print(img.shape)
     model = torch.hub.load('ultralytics/yolov5', 'custom',
                            path='best.pt', device='cpu')
     results = model(img)
@@ -25,24 +34,24 @@ def convert_img_to_array(img):
     
     m = results.to_numpy() #Transformar la matrix panda a numpy
     print(m)
-    print(type(m))
+    #print(type(m))
     
     information = []
 
     # Extraer las dimensiones de la imagen a usar
     y, x = img.shape[:2]
-    print('\nEl ancho: ' + str(x) + ' y el alto: ' + str(y) + '\n')
+    #print('\nEl ancho: ' + str(x) + ' y el alto: ' + str(y) + '\n')
 
     # Tranformacion de pixel para ingresar los datos en el OCR
-    i = 0
+    #i = 0
     for i in range(len(m)):
-        print('Fila de contador numero ' + str(i))
+       # print('Fila de contador numero ' + str(i))
         xmin = m[i, 0]
         ymin = m[i, 1]
         xmax = m[i, 2]
         ymax = m[i, 3]
-        print('Punto Xmin: ' + str(xmin) + '\nPunto Ymin: ' + str(
-            ymin) + '\nXmax: ' + str(xmax) + '\nymax: ' + str(ymax) + '\n')
+       # print('Punto Xmin: ' + str(xmin) + '\nPunto Ymin: ' + str(
+        #    ymin) + '\nXmax: ' + str(xmax) + '\nymax: ' + str(ymax) + '\n')
 
         x0 = ceil(xmin)
         y0 = ceil(ymin)
@@ -50,15 +59,17 @@ def convert_img_to_array(img):
         x1 = ceil(xmax)
         y1 = ceil(ymax)
 
-        print(x0, y0, x1, y1)
+       #print(x0, x1, y0, y1)
         
-        cortado = img[x0:x1, y0:y1]
+        cortado = img[y0:y1 , x0:x1]
+        #cv2.imshow('image window', cortado)
+        
         cut = ocr(cortado)
 
         information.append(cut)
-        i += 1
+       # i += 1
 
-    print('Informacion general\n' + str(information))
+    #print('Informacion general\n' + str(information))
 
 
 if __name__ == "__main__":
